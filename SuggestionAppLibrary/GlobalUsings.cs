4cab9f8bb13113dc9b690ba139aa90fe2beb33c3
Grzegorz Szewczyk <g.szewczyk196@gmail.com>
2022-06-21 23:44:22 +0200

    10 - Data Access classes

--- SuggestionAppLibrary/GlobalUsings.cs
+++ SuggestionAppLibrary/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using SuggestionAppLibrary.Models;
 global using MongoDB.Bson.Serialization.Attributes;
 global using MongoDB.Bson;
+global using MongoDB.Driver;
+global using Microsoft.Extensions.Caching.Memory;
